eb5140bdd8ed7ff04eb04d078824b8707829da9c
dani <85476608+dain9812@users.noreply.github.com>
2022-04-28 05:01:45 +0900

    refactor: Divide menu li data

---
+++ src/components/MenuData.js
@@ -0,0 +1,22 @@
+export const MenuData = [
+  {
+    title: "hacker news",
+    path: "/"
+  },
+  {
+    title: "show",
+    path: "/show"
+  },
+  {
+    title: "ask",
+    path: "/ask"
+  },
+  {
+    title: "jobs",
+    path: "/jobs"
+  },
+  {
+    title: "about",
+    path: "/about"
+  }
+];
